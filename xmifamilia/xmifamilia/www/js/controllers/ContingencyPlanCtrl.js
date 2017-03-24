13664f8bfeb08075d8df1b6282e2d969156e2ab8
AlbertoBlas <identidadwebmx@gmail.com>
2017-03-24 02:10:32 -0600

    Se arregla el choise del plan

--- xmifamilia/xmifamilia/www/js/controllers/ContingencyPlanCtrl.js
+++ xmifamilia/xmifamilia/www/js/controllers/ContingencyPlanCtrl.js
@@ -1,4 +1,9 @@
 app.controller('ContingencyPlanCtrl', function ($scope, $stateParams,$state) {
+	
+	$scope.choice = 'A';
+	
+	
+	console.log()
 	$scope.goTo = function (param) {
 		$state.go(param);
 		console.log(param)
